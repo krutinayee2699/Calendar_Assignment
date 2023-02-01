c64c63b9e61a7fb51e3a9e6a010b4c6f64ca2465
krutinayee2699 <krutinayee2699@gmail.com>
2023-02-01 20:30:04 +0530

    first commit

---
+++ Calendar2_KrutiNayee/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar2_KrutiNayee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int i, j, k = -1;
+                Console.WriteLine(" Su  Mo  Tu  We  Th  Fr  Sa");
+
+                for (i = 1; i <= 31; i = i + 7)
+                {
+
+                    for (j = 1; j <= 7; j++)
+                    {
+                        if (k <= 0)
+                            Console.Write("    ");
+                        else if (k > 0 & k < 10)
+                            Console.Write("  " + k + " ");
+                        else if (k == 32)
+                            break;
+                        else
+                            Console.Write("  " + k);
+                        k++;
+
+                    }
+                    Console.WriteLine("\n");
+                    Console.ReadKey();
+                }
+
+            }
+        }
+    }
+}
